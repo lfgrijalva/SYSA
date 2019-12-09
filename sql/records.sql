8d6615060009b1a05ec0c1694cc4d8e2416ca05f
Mathew Migliore <mathew.migliore@dcmail.ca>
2019-12-09 18:01:28 -0500

    Added data to the records and users tables

--- sql/records.sql
+++ sql/records.sql
@@ -23,17 +23,16 @@ CREATE TABLE records(
 
 
 insert into records(record_id,student_id,name,gned,itce,netd,oop,syde,sysa,webd,gpa,comments ) VALUES(
-	1,
-	100456789,
-	'Test',
-    50,
-    50,
-    50,
-    50,
-    50,
-    50,
-    50,
-    2.5,
-    'Nothing Special'
+	1, 100456789, 'test', 50, 50, 50, 50, 50, 50, 50, 2.50, 'Nothing Special',
+    2, 100362514, 'Mathew Migliore', 55, 66, 77, 88, 99, 22, 33, 2.43, 'Could have done better',
+    1, 100456789, 'John Doe', 45, 55, 65, 75, 85, 95, 100, 3.00, 'Nothing Special',
+    3, 100695847, 'Kevin Romero', 60, 70, 80, 85, 86, 87, 90, 3.93, 'Did well',
+    4, 100369857, 'Luis Grijalva', 22, 32, 52, 62, 72, 82, 92, 2.21, 'Teacher was not good on first course',
+    5, 100321456, 'Ryan Smith', 63, 66, 69, 72, 75, 78, 81, 3.00, 'Average grades',
+    6, 100695871, 'Anthony Doe', 84, 87, 90, 93, 96, 99, 100, 4.79, 'Tried hard this semester',
+    7, 10362495, 'Tristan Segovia', 52, 64, 76, 88, 55, 57, 59, 2.21, 'I need to study more',
+    8, 100214785, 'Darren Puff', 60, 61, 62, 63, 64, 65, 66, 2.14, 'I need to go to class more',
+    9, 100369854, 'Joe Moncada', 71, 72, 73, 74, 75, 76, 77, 3.21, 'Semester was alright',
+    10, 100589454, 'Grace Migliore', 81, 83, 51, 53, 90, 50, 65, 2.64, 'I understand some classes much better than others'
     );
 	
--- sql/users.sql
+++ sql/users.sql
@@ -11,7 +11,16 @@ CREATE TABLE users(
 );
 
 INSERT INTO users VALUES(
-	'jdoe',
-	md5('password'),
-	123);
+	'jdoe', md5('password'), 123,
+	'mmigliore', md5('schoolpass'), 234,
+	'kromero', md5('testpass'), 345,
+	'lgrijalva', md5('aihub'), 456,
+	'rsmith', md5('something'), 567,
+	'adoe', md5('camping'), 678,
+	'tsegovia', md5('classroom'), 789,
+	'dpuff', md5('towel22'), 987,
+	'jmoncada', md5('metro'), 876,
+	'gmigliore', md5('train'), 765
+	
+	);
 	
